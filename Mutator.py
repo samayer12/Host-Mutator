31992f61f38b24ce25edcaaee3933a2a54797465
kooki3monst4r <matthew.aust@icloud.com>
2016-08-08 14:27:29 -0400

    Maybe it'll work if I do a flowmod rather than packet out

--- Mutator.py
+++ Mutator.py
@@ -124,15 +124,15 @@ class Mutator(app_manager.RyuApp):
                 actions = [parser.OFPActionSetField(arp_tpa='10.131.1.2'), parser.OFPActionOutput(out_port)]
 
                 # install a flow to avoid packet_in next time
-                # if out_port != ofproto.OFPP_FLOOD:
-                    # match = parser.OFPMatch(in_port=in_port, eth_type=0x806, arp_tpa='10.131.1.2')
+                if out_port != ofproto.OFPP_FLOOD:
+                    match = parser.OFPMatch(in_port=in_port, eth_type=0x806, arp_tpa='10.131.1.2')
                     # verify if we have a valid buffer_id, if yes avoid to send both
                     # flow_mod & packet_out
-                    # if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                    #     self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                    #     return
-                    # else:
-                    #     self.add_flow(datapath, 1, match, actions)
+                    if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                        self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                        return
+                    else:
+                        self.add_flow(datapath, 1, match, actions)
                 data = None
                 self.logger.info(msg.buffer_id)
                 if msg.buffer_id == ofproto.OFP_NO_BUFFER:
@@ -147,18 +147,18 @@ class Mutator(app_manager.RyuApp):
                 actions = [parser.OFPActionSetField(arp_spa='10.131.1.5'), parser.OFPActionOutput(out_port)]
 
                 # install a flow to avoid packet_in next time
-                # if out_port != ofproto.OFPP_FLOOD:
-                #     match = parser.OFPMatch(in_port=in_port, eth_type=0x806, arp_spa = '10.131.1.2')
+                if out_port != ofproto.OFPP_FLOOD:
+                    match = parser.OFPMatch(in_port=in_port, eth_type=0x806, arp_spa = '10.131.1.2')
                     # TODO: for some reason the buffer is not showing up as empty, so I'm getting an error about an empty buffer. Maybe fix later
-                    #verify if we have a valid buffer_id, if yes avoid to send both
-                    #flow_mod & packet_out
-                    # if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                    #     self.add_flow(datapath, 1, match, actions)
-                    #
-                    #     # self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                    #     return
-                    # else:
-                    #     self.add_flow(datapath, 1, match, actions)
+                    # verify if we have a valid buffer_id, if yes avoid to send both
+                    # flow_mod & packet_out
+                    if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                        self.add_flow(datapath, 1, match, actions)
+
+                        # self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                        return
+                    else:
+                        self.add_flow(datapath, 1, match, actions)
                 data = None
                 if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                     data = msg.data
