ee14cf796d3547095b92eddddb52e29739e6096e
kooki3monst4r <matthew.aust@icloud.com>
2016-08-08 13:49:46 -0400

    What is my buffer ID?

--- Mutator.py
+++ Mutator.py
@@ -133,8 +133,10 @@ class Mutator(app_manager.RyuApp):
                     # else:
                     #     self.add_flow(datapath, 1, match, actions)
                 data = None
+                self.logger.info(msg.buffer_id)
                 if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                     data = msg.data
+                    self.logger.info("no buffer 1")
 
                 out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                           in_port=in_port, actions=actions, data=data)
